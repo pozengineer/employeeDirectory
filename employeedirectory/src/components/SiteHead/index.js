de924b46ba46d66c822838d49d1670ce2a0ab138
Vegeta <56867717+pozengineer@users.noreply.github.com>
2020-04-13 10:00:56 +1000

    initial upload to gitHub

---
+++ employeedirectory/src/components/SiteHead/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+function SiteHead() {
+    return (
+        <div className="wrapper">
+            <div className="title">
+                Employee Directory
+            </div>
+            <br></br>
+            <div className="tagline">
+                Access Employee Information Whenever/ Wherever!
+            </div>
+        </div>
+    );
+}
+
+export default SiteHead;
